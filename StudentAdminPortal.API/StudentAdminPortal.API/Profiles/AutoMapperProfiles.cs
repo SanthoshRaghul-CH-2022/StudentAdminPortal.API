d875ae80d97ca9c38338039e2be169413abda76c
SanthoshRaghul-CH-2022 <srs.kanini@gmail.com>
2023-01-26 19:43:38 +0530

    API Code Committed

---
+++ StudentAdminPortal.API/StudentAdminPortal.API/Profiles/AutoMapperProfiles.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using StudentAdminPortal.API.DataModels;
+using StudentAdminPortal.API.DomainModels;
+
+namespace StudentAdminPortal.API.Profiles
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<DataModels.Student, DomainModels.Student>();
+            CreateMap<DataModels.Gender, DomainModels.Gender>();
+            CreateMap<DataModels.Address, DomainModels.Address>();
+        }
+    }
+}
